5fdf55e2dd9c59859b4df70655f6db4444a32000
Albeles <96035679+Albeles@users.noreply.github.com>
2022-12-21 17:13:05 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
+customhost = "employee.cczntjdgjrjb.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
